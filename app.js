67a3fc26ed47c5575b34c6570c1a6c1f949f48d5
Elliot J. Voris <elliot@voris.me>
2021-11-15 22:20:53 -0600

    creating the (de)serializeUser functions
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- app.js
+++ app.js
@@ -27,6 +27,18 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Create (de)serializeUser functions
+passport.serializeUser((user, done) => {
+  done(null, user._id)
+})
+
+passport.deserializeUser((id, done) => {
+  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+  //   done(null, null)
+  // })
+  done(null, null)
+})
+
 app.route('/').get((req, res) => {
   res.render(`${process.cwd()}/views/pug`, {
     title: 'Hello',
