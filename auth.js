85bcc0713578c49a3c1ed44ec5f86ce004db20ef
Elliot J. Voris <elliot@voris.me>
2021-11-16 01:47:59 -0600

    The github auth is now working!
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- auth.js
+++ auth.js
@@ -18,13 +18,41 @@ module.exports = function (app, myDataBase) {
     })
   }))
 
+  let cbURL = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080'
+    : 'https://boilerplate-advancednode.elliotfriend.repl.co'
   // set up the passport-github2 strategy
   passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: 'https://boilerplate-advancednode.elliotfriend.repl.co/auth/github/callback'
+    callbackURL: `${cbURL}/auth/github/callback`
   }, (accessToken, refreshToken, profile, done) => {
     console.log(profile)
+    myDataBase.findOneAndUpdate(
+      { id: profile.id },
+      {
+        $setOnInsert: {
+          id: profile.id,
+          name: profile.displayName || 'John Doe',
+          photo: profile.photos[0].value || '',
+          email: Array.isArray(profile.emails)
+            ? profile.emails[0].value
+            : 'No public email',
+          created_on: new Date(),
+          provider: profile.provider || ''
+        },
+        $set: {
+          last_login: new Date()
+        },
+        $inc: {
+          login_count: 1
+        }
+      },
+      { upsert: true, new: true },
+      (err, doc) => {
+        return done(null, doc.value)
+      }
+    )
   }))
 
   // Create (de)serializeUser functions
