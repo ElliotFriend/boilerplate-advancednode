fb24a85b3937b2f7bc9005774f16bbf57eb75e8c
Elliot J. Voris <elliot@voris.me>
2021-11-15 23:16:26 -0600

    Add the logout route and a 404 handler
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- app.js
+++ app.js
@@ -73,6 +73,17 @@ myDB(async client => {
     }
   )
 
+  app.route('/logout').get((req, res) => {
+    req.logout()
+    res.redirect('/')
+  })
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found')
+  })
+
   // Create (de)serializeUser functions
   passport.serializeUser((user, done) => {
     done(null, user._id)
