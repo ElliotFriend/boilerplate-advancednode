1d62f015763d3575ce4cf0ada74d3095ad29e808
Elliot J. Voris <elliot@voris.me>
2021-11-16 01:15:00 -0600

    Screwed up some of the github auth syntax
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- routes.js
+++ routes.js
@@ -38,17 +38,14 @@ module.exports = function (app, myDataBase) {
     res.redirect('/')
   })
 
-  app.route('/auth/github').get((req, res) => {
-    passport.authenticate('github')
-  })
+  app.route('/auth/github').get(passport.authenticate('github'))
 
-  app.route('/auth/github/callback').get((req, res) => {
-    passport.authenticate('github', {
+  app.route('/auth/github/callback').get(passport.authenticate('github', {
       failureRedirect: '/'
     }), (req, res) => {
       res.redirect('/profile')
     }
-  })
+  )
 
   app.route('/register').post((req, res, next) => {
     myDataBase.findOne({ username: req.body.username }, (err, user) => {
